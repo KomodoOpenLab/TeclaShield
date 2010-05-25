b53e5186764b3bf5e1e3dd2999534ec794422467
Jorge Silva <jsilva.mx@gmail.com>
2010-05-25 00:01:43 -0400

    Updated bluetooth code to include BlueSMiRF board

--- clients/maemo/maemo_meadl_client.py
+++ clients/maemo/maemo_meadl_client.py
@@ -1,4 +1,5 @@
 #! /user/bin/python
+# -*- coding: utf-8 -*-
 
 # mEADL, mobile Electronic Aids for Daily Living
 # Copyright (c) 2009, University of Toronto
@@ -26,33 +27,29 @@
 import os
 from bluetooth import *
 
-### Use this code to scan for devices
+### Use this code to scan for devices ###
 #print "Looking for bluetooth devices in range..."
 #nearby_devices = discover_devices(lookup_names = True)
 #print "found %d devices" % len(nearby_devices)
-
 #for name, addr in nearby_devices:
-# print "%s - %s" % (addr, name)
-###
+#	print "%s - %s" % (addr, name)
+#########################################
 
 def UI_Print(msg):
-    print msg
-    cmd = 'espeak -w temp.wav "' + msg + '"'
-    os.system(cmd)
-    #os.system('mplayer temp.wav') # Better for full Linux distros
-    os.system('play-sound temp.wav')  # Best for Maemo
+	print msg
+	cmd = 'espeak -w temp.wav "' + msg + '"'
+	os.system(cmd)
+	os.system('mplayer temp.wav') # Better for full Linux distros
+	#os.system('play-sound temp.wav')  # Best for Maemo
 
 UI_Print('Connecting to bluetooth interface...')
 client_socket = BluetoothSocket( RFCOMM )
-client_socket.connect(('00:07:80:82:1E:E6', 1))
+#client_socket.connect(('00:07:80:82:1E:E6', 1)) #ArduinoBT
+client_socket.connect(('00:06:66:02:CB:75', 1)) #BlueSMiRF
 UI_Print('Bluetooth interface connection established.')
 
 while 1:
-    data = ord(client_socket.recv(1)) # Receive only one byte at a time
-    print "Received:", data # Should turn this into a debug message
-    if data == 48:
-        UI_Print('Button Pressed')
-    elif data == 49:
-        UI_Print('Button Released')
+	data = ord(client_socket.recv(1)) # Receive only one byte at a time
+	print "0x%0x" % data
 
 client_socket.close()
