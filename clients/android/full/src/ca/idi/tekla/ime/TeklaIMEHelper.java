be4e889081dcb52e097c93c4fc3ae86081bf85e8
Jorge Silva <jorge.silva@utoronto.ca>
2010-08-24 18:47:13 -0400

    Minor fixes for upload to market

--- clients/android/full/src/ca/idi/tekla/ime/TeklaIMEHelper.java
+++ clients/android/full/src/ca/idi/tekla/ime/TeklaIMEHelper.java
@@ -1,13 +1,7 @@
 package ca.idi.tekla.ime;
 
-import ca.idi.tekla.R;
 import ca.idi.tekla.TeklaIMESettings;
-import ca.idi.tekla.sep.SwitchEventProvider;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.inputmethodservice.KeyboardView;
 import android.os.Handler;
@@ -19,14 +13,11 @@ public class TeklaIMEHelper {
 	private static final int REDRAW_KEYBOARD = 99999; //this is a true arbitrary number.
 	
 	private KeyboardView mKeyboardView;
-    private NotificationManager mNotificationManager;
     private Context context;
 
 
 	public TeklaIMEHelper (Context context) {
 		this.context = context;
-		mNotificationManager = (NotificationManager)
-			this.context.getSystemService(Context.NOTIFICATION_SERVICE);
 	}
 	
 	private SharedPreferences retrievePreferences() {
